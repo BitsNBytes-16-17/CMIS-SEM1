e08eb7b80b138536e41b3d77ea00bcb3018d3ee0
Chamod Shehanka <hcsperera@outlook.com>
2018-06-15 23:29:41 +0530

    Created q2

---
+++ assignment 1/q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("%d \n",5 + (3 * 2));
+    printf("%d \n",(5 + 3) * 2);
+    printf("%d \n",7 + 3 - 2);
+    printf("%d \n",7 + (3 - 2));
+    printf("%d \n",17 + 3 * 2 / 2);
+    printf("%d \n",17 + 3 * (2 / 2));
+
+    return 0;
+}

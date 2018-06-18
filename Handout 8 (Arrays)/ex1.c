5cf61128967bcc8af0f0b042c2348ba8491bdcaa
Chamod Shehanka <hcsperera@outlook.com>
2018-06-18 06:31:14 -0400

    Update

--- Handout 8 (Arrays)/ex1.c
+++ Handout 8 (Arrays)/ex1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
+	int i;
 	char letters[5] = {'A', 'B', 'C', 'D', 'E'};
 
 	printf("%c\n", letters[0]);// Prints A
@@ -11,6 +12,14 @@ int main(void){
 
 	printf("Input charatcter : ");
 	scanf(" %c", &letters[3]); //To replace 4th element with input through keyboard
+
+
+	char array[] = {'a','b','c'};
+
+	for(i = 0;i < 3;i++){
+		printf("Input number : ");
+		scanf("%d", &array[i]);
+	}
 
 	return 0;
 

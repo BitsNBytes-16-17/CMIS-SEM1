89b7aea1571184dbc95602ad830e6c682174426e
Chamod Shehanka <hcsperera@outlook.com>
2018-06-18 07:59:46 -0400

    Ex5

---
+++ Handout 8 (Arrays)/ex5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int i,j = 4;
+	int array[5] = {10,5,16,35,500};
+	int reverseArray[5];
+
+	for(i = 0;i < 5;i++){
+		reverseArray[i] = array[j];
+		j--;
+	}
+	
+	printf("Reverse Array = [");
+	for(i = 0;i < 5;i++){
+		printf("%d",);
+	}
+}
